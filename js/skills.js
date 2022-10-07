fb345cebe1cecb4941b4135cd2e5b43ddb8a958e
Devon Morden <devon.r.morden@gmail.com>
2022-10-07 09:35:48 -0400

    added btn parameter to showContent to dynavically set the class to .current-page

--- js/skills.js
+++ js/skills.js
@@ -1,5 +1,7 @@
 const rootCauseAnalysis = document.getElementById('content-root-cause-analysis');
 
+const rootCauseAnalysisBtn = document.getElementById('btn-root-cause-analysis');
+
 const uxDesign = document.getElementById('content-ux-design');
 
 const projectManagement = document.getElementById('content-project-management');
@@ -11,11 +13,12 @@ const marketing = document.getElementById('content-marketing');
 const instructionalDesign = document.getElementById('content-instructional-design');
 
 /*shows content when target button is clicked*/
-function showContent(text){
+function showContent(text, btn){
     let elements = document.querySelectorAll('.content')
     elements.forEach((element) => {
       element.style.display = 'none';
     })
     text.style.display = 'block';
+    btn.setAttribute('class', 'current-page');
   }
 
