d67eae86198a1011a17b9c32ed569be7d720c5a9
Devon Morden <devon.r.morden@gmail.com>
2022-10-06 20:17:05 -0400

    set variables for content articles

---
+++ js/skills.js
@@ -0,0 +1,12 @@
+const rootCauseAnalysis = document.getElementById('content-root-cause-analysis');
+
+const uxDesign = document.getElementById('content-ux-design');
+
+const projectManagement = document.getElementById('content-project-management');
+
+const webDesign = document.getElementById('content-web-design');
+
+const marketing = document.getElementById('content-marketing');
+
+const instructionalDesign = document.getElementById('content-instructional-design');
+
