64f3ea7d328836a0b117592e9fc9187bbe8fede6
Devon Morden <devon.r.morden@gmail.com>
2022-10-07 14:56:31 -0400

    updated showContent function so only one button would be selected at a time. Also created class 'current-button' to use for the buttons, rather than 'current-page'.

--- js/skills.js
+++ js/skills.js
@@ -4,22 +4,34 @@ const rootCauseAnalysis = document.getElementById('content-root-cause-analysis')
 const rootCauseAnalysisBtn = document.getElementById('btn-root-cause-analysis');
 
 const uxDesign = document.getElementById('content-ux-design');
+const uxDesignBtn = document.getElementById('btn-ux-design');
 
 const projectManagement = document.getElementById('content-project-management');
+const projectManagementBtn = document.getElementById('btn-project-management');
 
 const webDesign = document.getElementById('content-web-design');
+const webDesignBtn = document.getElementById('btn-web-design');
 
 const marketing = document.getElementById('content-marketing');
+const marketingBtn = document.getElementById('btn-marketing');
 
 const instructionalDesign = document.getElementById('content-instructional-design');
+const instructionalDesignBtn = document.getElementById('btn-instructional-design');
 
-/*shows content when target button is clicked*/
-function showContent(text, btn){
-    let elements = document.querySelectorAll('.content')
-    elements.forEach((element) => {
-      element.style.display = 'none';
+const content = document.querySelectorAll('.content');
+const buttons = document.querySelectorAll('.button');
+
+/*shows content when button is clicked*/
+function showContent(element, btn){
+  content.forEach((element) => {
+    element.style.display = 'none';
     })
-    text.style.display = 'block';
-    btn.setAttribute('class', 'current-page');
-  }
+  buttons.forEach((button) => {
+    button.classList.remove('current-button')
+  })
+  element.style.display = 'block';
+  if (element.style.display === 'block') {
+    btn.setAttribute('class', 'current-button')
+  } 
+};
 
