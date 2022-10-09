53e622cce32d7ed33a334cb21d0e14714e76dbb4
Devon Morden <devon.r.morden@gmail.com>
2022-10-09 16:43:01 -0400

    can successfully navigate forward

--- js/work.js
+++ js/work.js
@@ -11,18 +11,30 @@ function nextSlide() {
   //get rid of project-visible
   //add project-hidden
   //show next section
-  for (let project of projects) { 
-    let i;
-    // var project = projects[i];
-    var activeSlide = project.classList.contains('project-visible');
-    if (activeSlide) {
+  // for (let project of projects) { 
+  //   console.log('start of for statement')
+  //   var activeSlide = project.classList.contains('project-visible');
+  //   console.log(project)
+  //   if (activeSlide) {
+  //     console.log('start of if statement')
+  //     project.classList.remove('project-visible');
+  //     project.classList.add('project-hidden')
+  //     projects.item(+1).classList.remove('project-hidden');
+  //     projects.item(+1).classList.add('project-visible');
+  //     console.log(projects.item(+1));
+  //   }
+  // };
+  for (let i = 0; i < projects.length; i++) {
+    let project = projects[i];
+    console.log(project);
+    if (project.classList.contains('project-visible')) {
       project.classList.remove('project-visible');
-      project.classList.add('project-hidden')
-      projects.item(+1).classList.remove('project-hidden');
-      projects.item(+1).classList.add('project-visible');
+      project.classList.add('project-hidden');
+      projects[i+1].classList.add('project-visible');
+      projects[i+1].classList.remove('project-hidden');
+      return;
     }
-    i++
-  };
+  }
 }
 
 function prevSlide() {
