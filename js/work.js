30f2144090d0533083e7c7bd9cbcc64e11470a91
Devon Morden <devon.r.morden@gmail.com>
2022-10-09 13:41:49 -0400

    made chevron buttons clickable; created project-hidden class with important display property

---
+++ js/work.js
@@ -0,0 +1,22 @@
+const left = document.getElementById('left-chevron');
+
+const right = document.getElementById('right-chevron');
+
+const projects = document.querySelectorAll('.project');
+
+// const inactiveSlide = inactiveSlides[i];
+
+function nextSlide() {
+  console.log('I heard that!');
+  //get rid of project-visible
+  //add project-hidden
+  //show next section
+  projects.forEach(() => { //go through all of the sections
+    let i = 0 //initialize index counter
+    let project = projects[i]; //initialize project: 
+  });
+}
+
+function prevSlide() {
+  console.log("Je l'ai entendu!");
+}
