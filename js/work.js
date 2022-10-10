18ab8fe5865b9d3da1939b91dd5c6f8f2538671c
Devon Morden <devon.r.morden@gmail.com>
2022-10-09 22:21:11 -0400

    added showDetails() to toggle details

--- js/work.js
+++ js/work.js
@@ -9,7 +9,6 @@ const projects = document.querySelectorAll('.project');
 function nextSlide() {
   for (let i = 0; i < projects.length; i++) {
     let project = projects[i];
-    console.log(project);
     if (project.classList.contains('project-visible')) {
       project.classList.remove('project-visible');
       project.classList.add('project-hidden');
@@ -21,16 +20,27 @@ function nextSlide() {
 }
 
 function prevSlide() {
-  console.log("Je l'ai entendu!");
   for (let i = 0; i < projects.length; i++) {
     let project = projects[i];
-    console.log(project);
     if (project.classList.contains('project-visible')) {
       project.classList.remove('project-visible');
       project.classList.add('project-hidden');
       projects[i-1].classList.add('project-visible');
       projects[i-1].classList.remove('project-hidden');
       return;
+    }
+  }
+}
+
+// const detailsArticles = document.querySelectorAll('.details');
+
+function showDetails() {
+  console.log('audivi');
+  for (let i = 0; i<projects.length; i++) {
+    let project = projects[i];
+    if (project.classList.contains('project-visible')) {
+      let details = document.querySelectorAll('.details');
+      details[i].classList.toggle('details-hidden');
     }
   }
 }
