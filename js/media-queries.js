dbe03a0b81963942d56aa4fc67d9a4b9d0e24c93
Devon Morden <devon.r.morden@gmail.com>
2022-10-17 13:15:50 -0400

    index.html; started style.css

--- js/media-queries.js
+++ js/media-queries.js
@@ -1,14 +1,14 @@
-const medScrn = window.matchMedia('min-width: 600px');
+const medScrnPlus = window.matchMedia('min-width: 600px');
 const smScrn = window.matchMedia('max-width: 559px')
 
-if (medScrn) {
-  document.getElementById('main-nav-details').open = true;
+if (medScrnPlus) {
+  document.getElementById('main-nav-disclosure-widget').open = true;
 }
 
-if (medScrn) {
+if (medScrnPlus) {
   document.getElementById('work-experience-details').open = true;
 }
 
-if (medScrn) {
+if (medScrnPlus) {
   document.getElementById('education-details').open = true;
 }
