b4e22501e79be76183f8492aaf995a94e7cc9a3d
Devon Morden <devon.r.morden@gmail.com>
2023-11-26 13:59:29 -0500

    Really this time, I think I fixed the js for the nav on mobile and made it noscript accessible without using noscript.

--- js/media-queries.js
+++ js/media-queries.js
@@ -1,14 +1,24 @@
 const medScrnPlus = window.matchMedia('(min-width: 600px)');
-const smScrn = window.matchMedia('max-width: 559px');
+const smScrn = window.matchMedia('(max-width: 559px)');
 const mainNavDisclosureWidget = document.getElementById('main-nav-disclosure-widget');
 
-if (medScrnPlus) {
-  mainNavDisclosureWidget.setAttribute("open", "");
+if (smScrn) {
+  mainNavDisclosureWidget.removeAttribute("open","");
 };
+
+/* function testScreen(media) {
+  if(media.matches) {
+    mainNavDisclosureWidget.removeAttribute("open", "")
+  };
+); */
+/* 
+if (medScrnPlus) {
+  mainNavDisclosureWidget.setAttribute("open", "")
+}; */
 
 if (medScrnPlus) {
   document.getElementById('work-experience-details').open = true;
-}
+};
 
 if (medScrnPlus) {
   document.getElementById('education-details').open = true;
